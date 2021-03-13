a0210ff940a1366c148c23e92933611f6981d831
B3Funky <tyutin.s@list.ru>
2021-03-13 16:01:37 +0300

    feat: add solve for functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+  if (!array || !array.length) return 0;
+  array.sort((a, b) => {
+      return a - b;
+  });
+  return array[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  if (!array || !array.length) return 0;
+  array.sort((a, b) => {
+      return b - a;
+  });
+  return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  return sum / array.length;
+};
